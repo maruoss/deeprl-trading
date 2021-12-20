838f4a63fdbbcd03ddb0f997061ead0393362f19
Jan Komisarczyk <jan.komisarczyk@gmail.com>
2021-12-20 17:37:59 +0000

    repo results

--- GAN/main.py
+++ GAN/main.py
@@ -1,3 +1,6 @@
-print("ds")
+#print("ds")
 # conda create -n "your environment name" python==3.8
 # conda activate "your environment"
+#conda activate siglib
+
+python train.py -use_cuda -total_steps 1000
